ed8289529934eb26bd7baabfefecb0792e7fdf85
Navin <navin007.a@gmail.com>
2021-01-31 12:51:53 +0530

    feat(tailwind): ✨  add tailwind play markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue": colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
