159f1a7ddf97a828a46e5d6ec0d5339bc4cf2672
Navin <navin007.a@gmail.com>
2021-02-03 21:46:35 +0530

    feat(font): ✨  add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 const colors = require("tailwindcss/colors")
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         "light-blue": colors.lightBlue,
         cyan: colors.cyan,
